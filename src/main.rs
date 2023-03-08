7bf495fe7b3285e29f4dfe7bf12c671909120b03
Hugo <hugo.fontaine@protonmail.com>
2023-03-08 23:02:39 +0100

    It works ! Can't believe I made it work so fast

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,37 @@
-use mnist::{labels_to_responses, load_images, load_labels};
+use mnist::{display, load_images, load_labels, select_specific_numbers};
+use rand::distributions::{Open01};
 use rand::thread_rng;
 use rand_distr::StandardNormal;
+use neural::adversarial::AdversarialNetworks;
 use neural::threaded::ThreadedNetwork;
 
 fn main() {
     let nb_of_batch = 1000;
     let nb_of_thread = 20;
     let batch_size = 3;
-    let learning_rate = 3.5f64;
+    let learning_rate = 3.5f32;
+    let mut rng = thread_rng();
 
 
-    let images_train = load_images("dataset/train-images.idx3-ubyte");
+    //let images_train = load_images("dataset/train-images.idx3-ubyte");
     let images_test = load_images("dataset/t10k-images.idx3-ubyte");
-    let label_train = load_labels("dataset/train-labels.idx1-ubyte");
+   // let label_train = load_labels("dataset/train-labels.idx1-ubyte");
     let label_test = load_labels("dataset/t10k-labels.idx1-ubyte");
-    let responses = labels_to_responses(&label_train);
+    //let responses = labels_to_responses(&label_train);
 
-
+    /*
     let mut rng = thread_rng();
     let network = ThreadedNetwork::new(vec![784, 50, 10], learning_rate, &mut rng, &StandardNormal, nb_of_thread);
     println!("Efficiency of the network before training : {}%", network.test(images_test.clone(), label_test.clone()) * 100f32);
     network.training(images_train, responses, nb_of_batch, batch_size);
     println!("Efficiency of the network after training : {}%", network.test(images_test, label_test) * 100f32);
+    */
+
+
+    let (ones_images, _ones_labels) = select_specific_numbers(images_test, label_test, vec![0]);
+    let mut generative_network = AdversarialNetworks::new(vec![1, 30, 784], vec![784, 10, 2], learning_rate, &mut rng, &StandardNormal);
+    generative_network.training(ones_images, 1000, 20, &mut rng, &Open01);
+    let test = generative_network.generate(1, &mut rng, &Open01);
+
+    display(&test, &vec![1], 0);
 }
